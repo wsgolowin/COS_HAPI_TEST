08bcdce7b0f967d07ec0d93fce0ec2c73cfc2802
Aleksey Golovin <ws.golovin@gmail.com>
2018-08-24 11:16:23 +0300

    START

--- server.js
+++ server.js
@@ -1,4 +1,37 @@
 "use strict";
 
-console.log( "[ test ]" )
-;
+const Hapi = require( "hapi" );
+const Path = require( "path" );
+// const routes       = require('./routes');
+// const plugins      = require('./plugins');
+
+const DEFAULT_HOST = "localhost";
+const DEFAULT_PORT = 8000;
+const RADIX = 10;
+
+const server = Hapi.server( {
+    host: process.env.HOST || DEFAULT_HOST,
+    port: parseInt( process.env.PORT, RADIX ) || DEFAULT_PORT,
+    routes: {
+        files: {
+            relativeTo: Path.join( __dirname, "public" )
+        }
+    },
+    app: {}
+} );
+
+
+const init = async() => {
+    try {
+    // await server.register(plugins);
+    // await server.register(routes);
+        await server.start();
+        console.log( `Server running at" ${server.info.uri}` );
+
+    } catch ( error ) {
+        console.log( "Error", error );
+    }
+
+};
+
+init();
